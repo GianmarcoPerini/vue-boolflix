ccbba78325bf4b3cef21895f474078d40a11be54
GianmarcoPerini <g.marco.perini@gmail.com>
2021-10-01 15:54:12 +0200

    api init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+
+
+  mounted(){
+  axios.get('https://api.themoviedb.org/3/movie/popular?api_key=3c83c09e6615af6bfc5db9dc0a3760a3')
+    .then(res => console.log(res.data.results)
+    )
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
